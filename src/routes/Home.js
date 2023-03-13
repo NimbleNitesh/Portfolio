aa49d73361723cf91cecbe170e268e2951b3063b
NimbleNitesh <76262941+NimbleNitesh@users.noreply.github.com>
2023-03-14 00:24:27 +0530

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Navbar from "../components/BootstrapNavbar";
 import Header from "../components/BootstrapHeader";
-//import AboutInfo from "../components/aboutInfo.jsx";
-//import ProjectInfo from "../components/projectInfo.jsx";
+
 
 const Home = () => {
 	return (
