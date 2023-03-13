48f6056072dab5e7145d5b4b1654f5b8c88c42aa
NimbleNitesh <76262941+NimbleNitesh@users.noreply.github.com>
2023-03-14 02:44:46 +0530

    Update BootstrapHeader.js

--- src/components/BootstrapHeader.js
+++ src/components/BootstrapHeader.js
@@ -1,26 +1,52 @@
 import React from "react";
-import Typed from "react-typed";
+import Typed from 'typed.js';
 import CV from "./Nitesh_Resume.pdf";
 
 const BootstrapHeader = () => {
-	return (
-		<div className="header-wrapper">
-		<div className="mainInfo">
-			<h1>Hi, my name is</h1>
-			<h2>Nitesh Srivastava</h2>
-			<h3>I'm a </h3>
-			<Typed 
-				className="typed-text"
-				strings={["Competitive Programmer", "Web Developer", "ML Enthusiast"]}
-				typeSpeed={100}
-				backSpeed={50}
-				loop
-			/>
-			<p>I am a highly motivated and ambitious person, always ready to try out new things. I am highly passionate about Competitive Programming and have been pursuing the sport for about 1.5 years. I am actively looking for summer internships in software engineering domain. </p>
-			<a href={CV} download className="CTA">Download CV </a>
-		</div>
-		</div>
-	)
+  // Create reference to store the DOM element containing the animation
+  const el = React.useRef(null);
+  // Create reference to store the Typed instance itself
+  const typed = React.useRef(null);
+  React.useEffect(() => {
+    const options = {
+      strings: [
+        'Competitive Programmer',
+        'Web Developer',
+        'ML Enthusiast'
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true
+    };
+    
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+    
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    }
+  }, [])
+
+
+
+
+  return (
+    <div className="header-wrapper">
+    <div className="mainInfo">
+      <h1>Hi, my name is</h1>
+      <h2>Nitesh Srivastava</h2>
+      <h3>I'm a      
+      <div className="type-text">
+        <span style={{ whiteSpace: 'pre' }} ref={el} />
+      </div>
+      </h3>
+      <p>I am a highly motivated and ambitious person, always ready to try out new things. I am highly passionate about Competitive Programming and have been pursuing the sport for about 1.5 years. I am actively looking for summer internships in software engineering domain. </p>
+      <a href={CV} download className="CTA">Download CV </a>
+    </div>
+    </div>
+  )
 }
 
 export default BootstrapHeader
